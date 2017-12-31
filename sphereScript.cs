c2b2e9ef4d1a13cb54daee236f2f6a075ea3a34c
Bucky Roberts <bucky@pornhub.gov>
2017-12-31 08:16:39 +0530

    first funtionality

---
+++ sphereScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class sphereScript : MonoBehaviour {
+
+	[Range(0,2000)]
+	public float forcex;
+	[Range(0,2000)]
+	public float forcey;
+
+	Rigidbody rb;
+	public float x=0f,y=0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//x = Input.GetAxis ("Horizontal");
+	//	y = Input.GetAxis ("Vertical");
+
+	}
+	void FixedUpdate()
+	{
+		RotateMe ();
+	}
+	void OnCollisionStay(Collision col)
+	{
+		if (col.gameObject.tag == "Accelerate")
+		{
+			forcey += 0.05f;
+			y += 0.05f;
+			AccelerateMe ();
+		}
+		else
+		if (col.gameObject.tag == "Rotate")
+		{
+			forcex +=1f;
+			x += 0.03f;
+			RotateMe ();
+		}
+		else
+		{
+			x -= 0.05f;
+			y -= 0.05f;
+		}
+	}
+	void AccelerateMe()
+	{
+		//rb.AddForce ();	
+		rb.velocity=new Vector3(y,0f,x)*Time.deltaTime*-forcey;
+	}
+	void RotateMe()
+	{
+		rb.velocity=new Vector3(y,0f,x)*Time.deltaTime*-forcey;
+		rb.AddTorque (new Vector3(y,0f,x)*Time.deltaTime*forcex);	
+	}
+}
